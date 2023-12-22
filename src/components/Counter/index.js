3c7ae5e5e2a0143e864e29348e4e1c3959c9765b
Naveen Dharavath <naveendharavathh@gmail.com>
2023-12-22 15:52:34 +0000

    debugg

---
+++ src/components/Counter/index.js
@@ -0,0 +1,34 @@
+import './index.css'
+import {Component} from 'react'
+
+class Counter extends Component {
+  state = {count: 0}
+
+  onDecrement = () => {
+    const {count} = this.state
+    this.setState({count: count - 1})
+  }
+
+  onIncrement = () => {
+    const {count} = this.state
+    this.setState({count: count + 1})
+  }
+
+  render() {
+    const {count} = this.state
+
+    return (
+      <div className="app-container">
+        <h1 className="count">Count {count}</h1>
+        <button className="button" onClick={this.onDecrement} type="button">
+          Increase
+        </button>
+        <button className="button" onClick={this.onIncrement} type="button">
+          Decrease
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Counter
